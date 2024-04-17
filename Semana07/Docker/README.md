eba428d988028decf4121d0631e71a69556d97ff
Marco Mialaret Junior <matmj@cin.ufpe.br>
2024-04-17 10:45:41 -0300

    Padronizacao dos nomes dos diretorios

---
+++ Semana07/Docker/README.md
@@ -0,0 +1,25 @@
+### Configuração do Mongo e Docker
+
+1. Nosso primeiro passo é garantir a instalação do docker no computador.
+
+2.  Obter a imagem do Mongo que será o molde para criarmos nossos containers. Para isto, executamos o comando abaixo.
+
+```(Bash)
+docker pull mongo
+```
+
+3. Para executar esta imagem você pode usar a linha abaixo. 
+
+```(Bash)
+docker run --name mongodb -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=root mongo
+```
+
+4. Instale a extensão MongoDB for VS Code. 
+
+5. Acesse a extensão (uma folha na barra lateral), clique em Add Connection e use as configurações:
+
+```(Bash)
+mongodb://root:root@localhost:27017/admin
+```
+
+Mais detalhes em: https://balta.io/blog/mongodb-docker
---
+++ Semana07/ETL/etl.py
@@ -0,0 +1,31 @@
+import yfinance as yf
+import json
+
+# Defina os pares das moedas no formato correto para forex
+symbols = ["USDBRL=X", "EURBRL=X", "USDEUR=X"]
+
+# Defina o período de busca (1 ano)
+start_date = "2023-04-16"
+end_date = "2024-04-10"
+
+# Crie um dicionário vazio para armazenar os dados
+data = {}
+
+# Para cada símbolo, extraia os dados e adicione ao dicionário
+for symbol in symbols:
+    # Baixe os dados históricos
+    historical_data = yf.download(symbol, start=start_date, end=end_date)
+    
+    # Verifique se os dados foram baixados com sucesso
+    if not historical_data.empty:
+        # Converta as chaves do DataFrame de Timestamp para string
+        historical_data.index = historical_data.index.strftime('%Y-%m-%d')
+        # Converta o DataFrame em um dicionário
+        symbol_data = historical_data.to_dict()
+
+        # Adicione os dados ao dicionário principal
+        data[symbol] = symbol_data
+
+# Salve o dicionário em um arquivo JSON
+with open("cotacoes.json", "w") as outfile:
+    json.dump(data, outfile, indent=4)
